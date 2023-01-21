3ba3b6ce4405899a8398daad6a309514d3ded34e
rgbgamingcarpet <39846175+rgbgamingcarpet@users.noreply.github.com>
2023-01-21 18:34:10 +0000

    Initial commit, aftershock version of TwitchEmotesV2 converted to an extension addon instead.

---
+++ main.lua
@@ -0,0 +1,100 @@
+local aftershockPack = {
+	["arrogantsum"] = "Interface\\AddOns\\TwitchEmotes_Aftershock\\emotes\\arrogantsum.tga:28:28",
+    ["barafeet"] = "Interface\\AddOns\\TwitchEmotes_Aftershock\\emotes\\barafeet.tga:28:28",
+    ["birdweird"] = "Interface\\AddOns\\TwitchEmotes_Aftershock\\emotes\\birdweird.tga:28:28",
+    ["capywine"] = "Interface\\AddOns\\TwitchEmotes_Aftershock\\emotes\\capywine.tga:28:28",
+    ["catree"] = "Interface\\AddOns\\TwitchEmotes_Aftershock\\emotes\\catree.tga:28:28",
+    ["catward"] = "Interface\\AddOns\\TwitchEmotes_Aftershock\\emotes\\catward.tga:28:28",
+    ["chargesum"] = "Interface\\AddOns\\TwitchEmotes_Aftershock\\emotes\\chargesum.tga:28:28",
+    ["crazysum"] = "Interface\\AddOns\\TwitchEmotes_Aftershock\\emotes\\crazysum.tga:28:28",
+    ["clownsum"] = "Interface\\AddOns\\TwitchEmotes_Aftershock\\emotes\\clownsum.tga:28:28",
+    ["chunkMunk"] = "Interface\\AddOns\\TwitchEmotes_Aftershock\\emotes\\chunkMunk.tga:28:28",
+    ["fatsum"] = "Interface\\AddOns\\TwitchEmotes_Aftershock\\emotes\\fatsum.tga:28:28",
+    ["fatrat"] = "Interface\\AddOns\\TwitchEmotes_Aftershock\\emotes\\fatrat.tga:28:28",
+    ["happybara"] = "Interface\\AddOns\\TwitchEmotes_Aftershock\\emotes\\happybara.tga:28:28",
+    ["jellymunk"] = "Interface\\AddOns\\TwitchEmotes_Aftershock\\emotes\\jellymunk.tga:28:28",
+    ["munkreee"] = "Interface\\AddOns\\TwitchEmotes_Aftershock\\emotes\\munkreee.tga:28:28",
+    ["munkBillGates"] = "Interface\\AddOns\\TwitchEmotes_Aftershock\\emotes\\munkBillGates.tga:28:28",
+    ["munkConcerned"] = "Interface\\AddOns\\TwitchEmotes_Aftershock\\emotes\\munkConcerned.tga:28:28",
+    ["munkDutch"] = "Interface\\AddOns\\TwitchEmotes_Aftershock\\emotes\\munkDutch.tga:28:58",
+    ["munkFeet"] = "Interface\\AddOns\\TwitchEmotes_Aftershock\\emotes\\munkFeet.tga:28:28",
+    ["munkGimmeloot"] = "Interface\\AddOns\\TwitchEmotes_Aftershock\\emotes\\munkGimmeloot.tga:28:28",
+    ["munkGulag"] = "Interface\\AddOns\\TwitchEmotes_Aftershock\\emotes\\munkGulag.tga:28:28",
+    ["munkHappy"] = "Interface\\AddOns\\TwitchEmotes_Aftershock\\emotes\\munkHappy.tga:28:28",
+    ["munkHmpf"] = "Interface\\AddOns\\TwitchEmotes_Aftershock\\emotes\\munkHmpf.tga:28:28",
+    ["munkHolUp"] = "Interface\\AddOns\\TwitchEmotes_Aftershock\\emotes\\munkHolUp.tga:28:28",
+    ["munkInappropriate"] = "Interface\\AddOns\\TwitchEmotes_Aftershock\\emotes\\munkInappropriate.tga:28:28",
+    ["munkJudging"] = "Interface\\AddOns\\TwitchEmotes_Aftershock\\emotes\\munkJudging.tga:28:28",
+    ["munkLoot"] = "Interface\\AddOns\\TwitchEmotes_Aftershock\\emotes\\munkLoot.tga:28:28",
+    ["munkNorway"] = "Interface\\AddOns\\TwitchEmotes_Aftershock\\emotes\\munkNorway.tga:28:28",
+    ["munkOMGomw"] = "Interface\\AddOns\\TwitchEmotes_Aftershock\\emotes\\munkOMGomw.tga:28:28",
+    ["munkPog"] = "Interface\\AddOns\\TwitchEmotes_Aftershock\\emotes\\munkPog.tga:28:28",
+    ["munkPolice"] = "Interface\\AddOns\\TwitchEmotes_Aftershock\\emotes\\munkPolice.tga:28:28",
+    ["munkPolish"] = "Interface\\AddOns\\TwitchEmotes_Aftershock\\emotes\\munkPolish.tga:28:28",
+    ["munkQuestion"] = "Interface\\AddOns\\TwitchEmotes_Aftershock\\emotes\\munkQuestion.tga:28:28",
+    ["munkStop"] = "Interface\\AddOns\\TwitchEmotes_Aftershock\\emotes\\munkStop.tga:28:28",
+    ["munkWut"] = "Interface\\AddOns\\TwitchEmotes_Aftershock\\emotes\\munkWut.tga:28:28",
+    ["reesum"] = "Interface\\AddOns\\TwitchEmotes_Aftershock\\emotes\\reesum.tga:28:28",
+    ["smirkbara"] = "Interface\\AddOns\\TwitchEmotes_Aftershock\\emotes\\smirkbara.tga:28:28",
+	["rgbStare"] = "Interface\\AddOns\\TwitchEmotes_Aftershock\\emotes\\rgbStare.tga:28:28",
+	["monkeyShock"] = "Interface\\AddOns\\TwitchEmotes_Aftershock\\emotes\\monkeyShock.tga:28:28",
+}
+
+local aftershockEmotes = {
+	["arrogantsum"] = "arrogantsum",
+    ["barafeet"] = "barafeet",
+    ["birdweird"] = "birdweird",
+    ["capywine"] = "capywine",
+    ["catree"] = "catree",
+    ["catward"] = "catward",
+    ["chargesum"] = "chargesum",
+    ["crazysum"] = "crazysum",
+    ["clownsum"] = "clownsum",
+    ["chunkMunk"] = "chunkMunk",
+    ["fatsum"] = "fatsum",
+    ["fatrat"] = "fatrat",
+    ["happybara"] = "happybara",
+    ["jellymunk"] = "jellymunk",
+    ["munkreee"] = "munkreee",
+    ["munkBillGates"] = "munkBillGates",
+    ["munkConcerned"] = "munkConcerned",
+    ["munkDutch"] = "munkDutch",
+    ["munkFeet"] = "munkFeet",
+    ["munkGimmeloot"] = "munkGimmeloot",
+    ["munkGulag"] = "munkGulag",
+    ["munkHappy"] = "munkHappy",
+    ["munkHmpf"] = "munkHmpf",
+    ["munkHolUp"] = "munkHolUp",
+    ["munkInappropriate"] = "munkInappropriate",
+    ["munkJudging"] = "munkJudging",
+    ["munkLoot"] = "munkLoot",
+    ["munkNorway"] = "munkNorway",
+    ["munkOMGomw"] = "munkOMGomw",
+    ["munkPog"] = "munkPog",
+    ["munkPolice"] = "munkPolice",
+    ["munkPolish"] = "munkPolish",
+    ["munkQuestion"] = "munkQuestion",
+    ["munkStop"] = "munkStop",
+    ["munkWut"] = "munkWut",
+    ["reesum"] = "reesum",
+    ["smirkbara"] = "smirkbara",
+	["rgbStare"] = "rgbStare",
+	["monkeyShock"] = "monkeyShock",
+	["monkeyshock"] = "monkeyShock",
+}
+
+Emoticons_RegisterPack("TwitchEmotes_Aftershock", aftershockEmotes, aftershockPack)
+
+local function tablelength(T)
+    local count = 0
+    for _ in pairs(T) do count = count + 1 end
+    return count
+end
+
+local i = tablelength(AllTwitchEmoteNames);
+for k, _ in pairs(aftershockPack) do
+    AllTwitchEmoteNames[i] = k;
+    i = i + 1;
+end
+
+table.sort(AllTwitchEmoteNames)
